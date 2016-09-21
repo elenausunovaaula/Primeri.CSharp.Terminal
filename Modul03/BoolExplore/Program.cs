1e17da5a7b1faaf1d1ca260b2e1f0faebce09509
Елена Узунова <eusunova@abv.bg>
2016-09-21 13:46:08 +0300

    Модул 03, Видео 07 - Примери с променливи bool-2

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -14,46 +14,36 @@ namespace BoolExplore
 			//Console.WriteLine(_test);
 
 			int[] _i = new int[] { 4, 3, 2 };
-			int _ui = Convert.ToInt32(Console.ReadLine());
+			int _ui = 0;    //Convert.ToInt32(Console.ReadLine());
+			string _input = "0";
 
-			//bool _check01 = _ui <= _i.Length;
-			//bool _check02 = _ui > 0;
+			Console.Write("Моля въведете индекс: ");
+			_input = Console.ReadLine();
 
-			//bool _exp01 = _check01 && _check02;      // Логическо и "&&"
-			//bool _exp02 = _check01 || _check02;		// Логическо или "||"
-			//bool _exp03 = !_check01;				// Отрицание "!"
 
-			//Console.WriteLine( "Логическо И: " + _exp01.ToString () );
-			//Console.WriteLine( "Логическо ИЛИ: " + _exp02.ToString () );
-			//Console.WriteLine("Отрицание на: " + _exp01.ToString() + ": " + _exp03 );
+			bool _check01 = int.TryParse( _input, out _ui );
+			bool _check02 = _ui <= _i.Length;
+			bool _check03 = _ui > 0;
 
-			//Console.WriteLine(_check01);
-			//Console.WriteLine(_check02);
-
-			//Console.WriteLine(_i[_ui - 1]);
-
-			//if (_ui > 0 && _ui <= _i.Length)
-			//{
-			//	Console.WriteLine(_i[_ui - 1]);
-			//}
-			//else {
-			//	Console.WriteLine("Невалиден индекс\n\n\n");
-			//}
-
-			if (_ui > 0 && _ui <= _i.Length)
+			if (_check01 && _check02 && _check03)
 			{
 				Console.WriteLine(_i[_ui - 1]);
 			}
-			else {
-				if (_ui > _i.Length)
-				{
-					Console.WriteLine("Индексът е прекалено голям\n\n\n");
-				}
-				if (_ui <= 0)
-				{
-					Console.WriteLine("Индексът е прекалено малък\n\n\n");
-				}
+
+			if ( !_check01)
+			{
+				Console.WriteLine( "Индексът трябва да бъде цяло число\n\n\n");
 			}
+
+			if (!_check02)
+			{
+				Console.WriteLine("Индексът e прекалено голям\n\n\n");
+			}
+			if (!_check03 && _check01)
+			{
+				Console.WriteLine("Индексът e прекалено малък\n\n\n");
+			}
+
 		}
 	}
 }
