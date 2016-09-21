cd5268017bb8ed1a0fd7bd90ac9d98bcff400c42
Елена Узунова <eusunova@abv.bg>
2016-09-21 12:33:38 +0300

    Модул 03, Видео 04 - Променливи от тип bool-Корекция

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -4,9 +4,24 @@ namespace BoolExplore
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main()
 		{
-			Console.WriteLine ("Hello World!");
+			//bool _b = true;			//true, false
+			//Console.WriteLine(_b);
+
+			            
+			//bool _test = 5 > 10;        //false
+			//Console.WriteLine(_test);
+
+			int[] _i = new int[] { 4, 3, 2 };
+			int _ui = Convert.ToInt32(Console.ReadLine ());
+
+			bool _check01 = _ui <= _i.Length;
+			bool _check02 = _ui > 0;
+			Console.WriteLine(_i[_ui - 1]);
+			Console.WriteLine(_check01);
+			Console.WriteLine(_check02);
 		}
 	}
 }
+
