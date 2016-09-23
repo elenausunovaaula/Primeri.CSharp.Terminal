318aa0b119fadef663136677be48c8821d448a9e
Елена Узунова <eusunova@abv.bg>
2016-09-23 14:02:15 +0300

    Модул 04, Видео 01 - Цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//for (int i = 5; i > 0; i++) 		// нараства с едно -> 1,2,3,4,5
+			//for (int i = 5; i > 0; i--)			//намалява с едно -> 5,4,3,2,1
+			//for (int i = 0; i < 5; i++)			// 1,2,3,4,5
+			for (int i = 0; i <= 5; i+=2)			//през едно ->0,2,4
+				
+				Console.WriteLine( i.ToString () );
+		}
+	}
+}
